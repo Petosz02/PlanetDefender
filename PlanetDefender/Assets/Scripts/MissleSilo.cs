0f4df821a3403780f08457a563e01812b2a1dc43
Petosz02 <petorgeri@gmail.com>
2021-06-28 21:50:52 +0200

    kis haladás

---
+++ PlanetDefender/Assets/Scripts/MissleSilo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleSilo : MonoBehaviour
+{
+    [SerializeField] private HomingMissle missle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FireMisle(Transform target){
+        HomingMissle firedMisle = Instantiate(missle, this.transform.position, this.transform.rotation);
+        firedMisle.target = target;
+    }
+}
---
+++ PlanetDefender/Assets/Scripts/ScriptableObjects/BuildingSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", menuName ="Scriptables/New Building")]
+public class BuildingSO : ScriptableObject
+{
+    public string buildingName;
+    public int buildingCost;
+    public int buildingHP;
+    public GameObject buildingPrefab;
+}
